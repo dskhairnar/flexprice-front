2ff5f26a7b7322985341d4aa32a652ef053e80ac
Hitesh Kumar Khatri <hiteshkumarkhatri7410@gmail.com>
2026-06-18 19:28:05 +0530

    fix: display API key name in delete confirmation dialog

--- src/pages/developer/developer.tsx
+++ src/pages/developer/developer.tsx
@@ -187,7 +187,7 @@ const DeveloperPage = () => {
 									await SecretKeysApi.deleteSecretKey(id);
 								}}
 								refetchQueryKey='secret-keys'
-								entityName={t('apiKeys.entityName')}
+								entityName={rowData?.name}
 								// edit={{
 								// 	enabled: true,
 								// 	onClick: () => {},
